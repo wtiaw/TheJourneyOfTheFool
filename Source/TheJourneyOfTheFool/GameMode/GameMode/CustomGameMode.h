1b0bb76474a6b695d6b2d177d87d5b4b6707213e
wtiaw <964226463@qq.com>
2021-12-30 19:32:20 +0800

    Init

---
+++ Source/TheJourneyOfTheFool/GameMode/GameMode/CustomGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CustomGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEJOURNEYOFTHEFOOL_API ACustomGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
