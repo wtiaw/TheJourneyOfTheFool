e8efc5cac57448567e9b95cd6c8c2a17139d307a
wtiaw <964226463@qq.com>
2021-12-30 20:14:20 +0800

    Init

---
+++ Source/TheJourneyOfTheFool/Charaters/PaperCharacterBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "PaperCharacterBase.generated.h"
+
+/**
+ * 基础人物模板
+ */
+UCLASS()
+class THEJOURNEYOFTHEFOOL_API APaperCharacterBase : public APaperCharacter
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, Category = "Character Attribute")
+	float MaxHealth;
+};
